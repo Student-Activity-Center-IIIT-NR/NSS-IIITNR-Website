6fcfe7f071c0b07613cd7f9969dcdeeaf714e2ae
Sudhanshu Tripathi <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-07-05 15:01:40 +0530

    merge

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { Blogs } from "./Components/Blogs/Blogs";
 import { Events } from "./Components/Events/Events";
 
-import { Footer } from "./Components/Footer";
+import { Footer } from "./Components/Footer.jsx";
 import { NSS } from "./Components/HomePage/NSS";
 import { Landing } from "./Components/HomePage/Landing";
-import { Navbar } from "./Components/Navbar";
+import { Navbar } from "./Components/Navbar.jsx";
 import { GoalsVision } from "./Components/HomePage/GoalsVision";
 import Gallery from "./gallery/Gallery";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Error from "./components/Error";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import Gallery from "./gallery/Gallery";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Error from "./components/Error";
+// import Footer from "./components/Footer";
+// import Navbar from "./components/Navbar";
+// import Gallery from "./gallery/Gallery";
 import Teams from "./teams/Teams";
 
 const App = () => {
